b2765d318c5cd3b01bc39859277102e125fbb33f
akshat31 <akshat.soni31@gmail.com>
2018-10-11 13:53:18 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var mongo = require('mongodb');
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://akshat31:friend4u@ds153869.mlab.com:53869/mydb',['customers']);
+var app = express();
+
+
+app.listen('4000' , ()=>{
+    console.log("server started on port 4000")
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.set('view engine','ejs');
+//app.set('views' , path.join(__dirname, 'views'))
+app.use( express.static( "views" ) );
+
+app.get( '/' , (req , res)=>{
+    db.customers.find(function (err, docs) {
+        res.render('index' , {
+            myname:'ENTER HERE:-',
+            user:docs
+        }); 
+    })
+    
+})
+
+app.post('/users/add' , function(req , res){
+    
+    var newEntry = {
+        name:req.body.first_name,
+        email:req.body.my_email,
+        address:req.body.appadd
+    }
+    db.customers.insert(newEntry , function(err , result){
+        if(err){ console.log(err);}
+        res.redirect('/');
+    })
+});
+
+app.post('/users/delete' , function(req , res){
+    
+    var newEntry = {
+        name:req.body.delete0
+    }
+    console.log(newEntry.name)
+    db.customers.remove({"name":newEntry.name} , null , function(err , result){
+        if(err){ console.log(err)}
+        res.redirect('/');
+    });
+    console.log(newEntry.name)
+});
+
+
+
